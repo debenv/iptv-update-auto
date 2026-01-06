deda6db217053b0f15076dd360598f3d84cc3a02
debenv <54020772+debenv@users.noreply.github.com>
2026-01-06 19:17:40 +0800

    Create update_unicom_m3u.sh

---
+++ update_unicom_m3u.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# 下载 unicom.m3u 文件
+curl -o unicom.m3u https://raw.githubusercontent.com/chilinha/tv/refs/heads/main/unicom.m3u
+
+# 将 rtp:// 替换为 http://192.168.33.3:4022/udp/
+sed -i 's|rtp://|http://192.168.33.3:4022/udp/|g' unicom.m3u
+
+# 将 logo 路径替换
+sed -i 's|https://gitee.com/chilinha/mytv/raw/master/|http://192.168.33.90:58418/tv/tvlogo/raw/branch/master/|g' unicom.m3u
+
+echo "处理完成，文件已保存为 unicom.m3u"
